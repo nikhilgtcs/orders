c775689e3f20fa39fffdc5950f2a84e9013de470
nikhilgtcs <nikhilgtcs@gmail.com>
2022-03-01 18:45:24 +0530

    run successfull

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer userid;
+	private String name;
+	private Integer orderno;
+	public Integer getUserid() {
+		return userid;
+	}
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getOrderno() {
+		return orderno;
+	}
+	public void setOrderno(Integer orderno) {
+		this.orderno = orderno;
+	}
+	
+	
+}
